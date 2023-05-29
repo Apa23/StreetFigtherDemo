945e0c2c85efd8cc77f78818cdac2077d9c88750
Andrés Aparicio <andres.apa.23@gmail.com>
2023-05-29 10:32:23 -0500

    Control input

--- Bushido tenkai/Assets/Scripts/Movement.cs
+++ Bushido tenkai/Assets/Scripts/Movement.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
-{   private Rigidbody2D rb2D;
+{
+    private PlayerControls _playerControls;
+    private Rigidbody2D rb2D;
     [Header("Movement")]
     [SerializeField]
     private float speed;
@@ -22,6 +25,22 @@ public class Movement : MonoBehaviour
 
     private Animator animator;
 
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+        _playerControls.Gameplay.Move.performed += ctx => ActionMove();
+        _playerControls.Gameplay.Jump.performed += ctx => ActionMove();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Gameplay.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -33,14 +52,14 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dirX = Input.GetAxis("Horizontal") * speed;//Toma la dirección de movimiento y le da velocidad.
-        animator.SetFloat("Speed",Mathf.Abs(dirX));//Envia al animator la velocidad del pnje.
+        dirX = Input.GetAxis("Horizontal") * speed;//Toma la direcciï¿½n de movimiento y le da velocidad.
+        animator.SetFloat("Speed", Mathf.Abs(dirX));//Envia al animator la velocidad del pnje.
 
-        
 
-        if (Input.GetButtonDown("Jump") && animator.GetBool("IsJumping")==false ) //Detecta salto
+
+        if (Input.GetButtonDown("Jump") && animator.GetBool("IsJumping") == false) //Detecta salto
         {
-            animator.SetBool(("IsJumping"),true);
+            animator.SetBool(("IsJumping"), true);
             Jump = true;
 
         }
@@ -48,17 +67,17 @@ public class Movement : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))//detecta atacar.
         {
             Attack = true;
-            
+
             if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1Animation"))
             {
-                // El jugador ha presionado el botón de ataque mientras se estaba reproduciendo la animación del primer ataque
+                // El jugador ha presionado el botï¿½n de ataque mientras se estaba reproduciendo la animaciï¿½n del primer ataque
                 CanAttackAgain = true;
             }
-            
+
 
 
         }
-            
+
 
 
     }
@@ -68,24 +87,28 @@ public class Movement : MonoBehaviour
 
         if (Attack)
         {
-            animator.SetBool("IsAttacking", true); //Inicia animación de ataque
+            animator.SetBool("IsAttacking", true); //Inicia animaciï¿½n de ataque
+            animator.SetInteger("ComboAttack", 1);
 
-            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f) // Verifica si la animación ya acabo.
+
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f) // Verifica si la animaciï¿½n ya acabo.
             {
                 if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1Animation") && CanAttackAgain) // Verifica si se puede realizar un segundo ataque.
                 {
-                    animator.SetBool("DobleAttack", true); //Inicia un segundo ataque.
-                    CanAttackAgain = false; // No se permite hacer otro ataque hasta que la animación termine.
+                    Debug.Log("Can attack");
+                    animator.SetInteger("ComboAttack", 2); //Inicia un segundo ataque.
+                    CanAttackAgain = false; // No se permite hacer otro ataque hasta que la animaciï¿½n termine.
                 }
                 else
                 {
                     Attack = false;
                     animator.SetBool("IsAttacking", false);
+                    animator.SetInteger("ComboAttack", 0);
                 }
                 if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack2Animation"))
                 {
-                    animator.SetBool("IsAttacking", false);  // desactiva el parámetro de ataque 1 en el animator
-                    animator.SetBool("DobleAttack", false);  // desactiva el parámetro de ataque 2 en el animator
+                    animator.SetBool("IsAttacking", false);  // desactiva el parï¿½metro de ataque 1 en el animator
+                    animator.SetInteger("ComboAttack", 0);  // desactiva el parï¿½metro de ataque 2 en el animator
                     Attack = false;
                     CanAttackAgain = false;
                 }
@@ -93,26 +116,26 @@ public class Movement : MonoBehaviour
         }
         animator.SetFloat("JumpVelocity", rb2D.velocity.y);//envia al animator la velocidad en componente y para transicionar.
         if (Jump)
-        {   
+        {
             ActionJump();//SALTA!
         }
         if (dirX != 0)
         {
             ActionMove(); //Muevete!
         }
-       
-      
-        
+
+
+
     }
     private void ActionJump()//Realiza el salto
     {
-        
-     
+
+
         Jump = false;
         rb2D.AddForce(new Vector2(0f, JumpForce));//Agregamos una fuerza vertical
 
     }
-    private void ActionMove()//Actualiza la posición del personaje.
+    private void ActionMove()//Actualiza la posiciï¿½n del personaje.
     {
         transform.position += new Vector3(dirX, 0, 0) * Time.fixedDeltaTime;
     }
@@ -122,11 +145,10 @@ public class Movement : MonoBehaviour
     {
         if (other.collider.CompareTag("Floor"))
         {
-            animator.SetBool("IsJumping", false);//desactiva el salto, para poder pasar a otra animación.
+            animator.SetBool("IsJumping", false);//desactiva el salto, para poder pasar a otra animaciï¿½n.
         }
     }
 
- 
+
 
 }
-      
